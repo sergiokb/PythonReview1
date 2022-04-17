46c5ea162472e6caee0a4c8d9e4102dc8dc8c529
sergiokb <sergiokb@yandex.ru>
2022-04-17 22:43:22 +0300

    Fixed as you wished

---
+++ drawing.py
@@ -0,0 +1,12 @@
+import pygame
+from colors import white
+
+font_name = pygame.font.match_font('arial')
+
+
+def draw_text(surf, text, size, x, y):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, white)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
